3c4aceedd901c2ef42ede90839536f4b6c50a68e
Ehren von Ibsch <ehrenwolf42@icloud.com>
2025-02-05 15:30:11 -0800

    Change min angle

--- src/main/java/com/spartronics4915/frc2025/Constants.java
+++ src/main/java/com/spartronics4915/frc2025/Constants.java
@@ -203,7 +203,7 @@ public final class Constants {
         public static final double kA = 0.0;
         
         //The values set here are placeholders for sim
-        public static final Rotation2d kMinAngle = Rotation2d.fromRotations(0);
+        public static final Rotation2d kMinAngle = Rotation2d.fromRotations(-1);
         public static final Rotation2d kMaxAngle = Rotation2d.fromRotations(1);
 
         public static final SlotConfigs kPIDConfigs = new SlotConfigs()
