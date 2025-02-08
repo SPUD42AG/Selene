44eda0fdd39ec73e7ae745a5af65603df84de4a9
Pointygreenskies <gnomeceratops@gmail.com>
2025-02-07 17:42:30 -0800

    Make DynamicsCommandFactory

---
+++ src/main/java/com/spartronics4915/frc2025/subsystems/coral/DynamicsCommandFactory.java
@@ -0,0 +1,8 @@
+package com.spartronics4915.frc2025.subsystems.coral;
+
+public class DynamicsCommandFactory {
+
+    private IntakeSubsystem intakeSubsystem;
+    private ElevatorSubsystem elevatorSubsystem;
+    private ArmSubsystem armSubsystem;
+}
