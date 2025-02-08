8a82b1cef7d25e0669099047fb6218fcfe8dd85d
Pointygreenskies <gnomeceratops@gmail.com>
2025-02-07 19:47:52 -0800

    add enum with robot positions

--- src/main/java/com/spartronics4915/frc2025/subsystems/coral/DynamicsCommandFactory.java
+++ src/main/java/com/spartronics4915/frc2025/subsystems/coral/DynamicsCommandFactory.java
@@ -5,4 +5,34 @@ public class DynamicsCommandFactory {
     private IntakeSubsystem intakeSubsystem;
     private ElevatorSubsystem elevatorSubsystem;
     private ArmSubsystem armSubsystem;
+
+    public DynamicsCommandFactory(ArmSubsystem armSubsystem, ElevatorSubsystem elevatorSubsystem, IntakeSubsystem intakeSubsystem) {
+        this.armSubsystem = armSubsystem;
+        this.elevatorSubsystem = elevatorSubsystem;
+        this.intakeSubsystem = intakeSubsystem;
+    }
+
+    public enum robotPosition {
+        INTAKEPOSITION(),
+        L4SCORE(),
+        STOW();
+        //add more or positions later
+
+       
+       
+    }
+
+        switch (moveToRobotPosition) {
+        //??maybe???
+        }
+    
+
+     /* ^^^^^here will be the function that takes in the enum and 
+     gives it the info of where it needs to be*/
+
+
+
+
+
+    
 }
