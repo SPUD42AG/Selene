735a34b21265d5caf19e1987966935323664bcb5
Pointygreenskies <gnomeceratops@gmail.com>
2025-02-10 19:10:29 -0800

    Add moveToStateCommand

--- src/main/java/com/spartronics4915/frc2025/subsystems/coral/DynamicsCommandFactory.java
+++ src/main/java/com/spartronics4915/frc2025/subsystems/coral/DynamicsCommandFactory.java
@@ -1,7 +1,7 @@
 package com.spartronics4915.frc2025.subsystems.coral;
-
-
+import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
@@ -17,25 +17,25 @@ public class DynamicsCommandFactory {
         this.intakeSubsystem = intakeSubsystem;
     }
 
-    public enum robotPosition {
+    public enum RobotPosition {
         INTAKE_POSITION(Rotation2d.fromDegrees(0), (0)),
         L4_SCORE(Rotation2d.fromDegrees(0), (0)),
         STOW(Rotation2d.fromDegrees(0), (0));
 
-        public Rotation2d angle;
-        public double meter;
+        public Rotation2d armAngle;
+        public double metersOfElevation;
         //add more or less positions later
 
-        private robotPosition(Rotation2d angle, double meter) {
-            this.angle = angle;
-            this.meter = meter;
+        private RobotPosition(Rotation2d armAngle, double metersOfElevation) {
+            this.armAngle = armAngle;
+            this.metersOfElevation = metersOfElevation;
         }
-        
         
     }
 
-    public Command goToState(robotPosition newState){
-        return Commands.none();
+    public Command moveToStateCommand(RobotPosition newState){
+       return Commands.parallel(armSubsystem.setMechanismAngleCommand(newState.armAngle),
+                          elevatorSubsystem.setSetPointCommand(newState.metersOfElevation));
     }
 
 }
