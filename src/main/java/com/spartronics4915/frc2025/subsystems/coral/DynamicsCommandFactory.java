26c8d139867d29881ed4e4f068eb44191f3d9fe5
otter502 <115671877+otter502@users.noreply.github.com>
2025-02-21 14:29:00 -0800

    Update L4 dynaPreset

--- src/main/java/com/spartronics4915/frc2025/subsystems/coral/DynamicsCommandFactory.java
+++ src/main/java/com/spartronics4915/frc2025/subsystems/coral/DynamicsCommandFactory.java
@@ -60,7 +60,7 @@ public class DynamicsCommandFactory {
         L1(0.1, Rotation2d.fromDegrees(47.900)),
         L2(0.0, Rotation2d.fromDegrees(47.900)),
         L3(Meters.of(0.23939+0.1524-0.0254).in(Meters), Rotation2d.fromDegrees(58.10311200000001)),
-        L4(Meters.of(1.25).in(Meters), Rotation2d.fromDegrees(15));
+        L4(Meters.of(1.23).in(Meters), Rotation2d.fromDegrees(14.33));
 
         private final DynamicsSetpoint setpoint;
 
