659ab7de5a282ed059fbd9d37ad89b6c972d75a3
sharkey300 <84826384+sharkey300@users.noreply.github.com>
2025-02-27 18:05:08 -0800

    Add L2 branch height to complex auto chooser

--- src/main/java/com/spartronics4915/frc2025/commands/ComplexAutoChooser.java
+++ src/main/java/com/spartronics4915/frc2025/commands/ComplexAutoChooser.java
@@ -53,7 +53,7 @@ public class ComplexAutoChooser {
         private void buildHeightChooser() {
             heightChooser.setDefaultOption("L4", BranchHeight.L4);
             heightChooser.addOption("L3", BranchHeight.L3);
-            // heightChooser.addOption("L2", BranchHeight.L2);
+            heightChooser.addOption("L2", BranchHeight.L2);
         }
 
         protected FieldBranch getFieldBranch() {
