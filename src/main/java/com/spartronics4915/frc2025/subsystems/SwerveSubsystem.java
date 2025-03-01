1fffc7779229fb4545a447299129ed6bb51d1cb7
sharkey300 <84826384+sharkey300@users.noreply.github.com>
2025-03-01 13:08:03 -0800

    Log speed of swerve drive to NetworkTables

--- src/main/java/com/spartronics4915/frc2025/subsystems/SwerveSubsystem.java
+++ src/main/java/com/spartronics4915/frc2025/subsystems/SwerveSubsystem.java
@@ -16,6 +16,7 @@ import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.networktables.DoublePublisher;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructPublisher;
 import edu.wpi.first.math.numbers.N1;
@@ -38,6 +39,7 @@ public class SwerveSubsystem extends SubsystemBase implements ModeSwitchInterfac
 
     private final StructPublisher<Pose2d> posePublisher = NetworkTableInstance.getDefault().getTable("logging").getStructTopic("pose", Pose2d.struct).publish();
     private final StructPublisher<ChassisSpeeds> shimPublisher = NetworkTableInstance.getDefault().getTable("logging").getStructTopic("shim", ChassisSpeeds.struct).publish();
+    private final DoublePublisher speedPublisher = NetworkTableInstance.getDefault().getTable("logging").getDoubleTopic("speed").publish();
 
     public SwerveSubsystem(SwerveDirectories swerveDir) {
 
@@ -177,6 +179,7 @@ public class SwerveSubsystem extends SubsystemBase implements ModeSwitchInterfac
     @Override
     public void periodic() {
         posePublisher.accept(getPose());
+        speedPublisher.accept(getSpeed());
     }
 
 }
