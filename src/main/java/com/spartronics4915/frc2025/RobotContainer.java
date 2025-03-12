e8ac1bc13758cbf0b9dea5d73a206c408e5430f2
Ehren von Ibsch <ehrenwolf42@icloud.com>
2025-03-11 18:13:14 -0700

    Initiall commit

--- src/main/java/com/spartronics4915/frc2025/RobotContainer.java
+++ src/main/java/com/spartronics4915/frc2025/RobotContainer.java
@@ -13,41 +13,17 @@ import com.spartronics4915.frc2025.Constants.ElevatorConstants.ElevatorSubsystem
 import com.spartronics4915.frc2025.Constants.IntakeConstants.IntakeSpeed;
 import com.spartronics4915.frc2025.Constants.Drive;
 import com.spartronics4915.frc2025.Constants.OI;
-import com.spartronics4915.frc2025.commands.Autos;
-import com.spartronics4915.frc2025.commands.ComplexAutoChooser;
-import com.spartronics4915.frc2025.commands.DynamicsCommandFactory;
-import com.spartronics4915.frc2025.commands.ElementLocator;
-import com.spartronics4915.frc2025.commands.VariableAutos;
-import com.spartronics4915.frc2025.commands.Autos.AutoPaths;
-import com.spartronics4915.frc2025.commands.DynamicsCommandFactory.DynaPreset;
-import com.spartronics4915.frc2025.commands.autos.AlignToReef;
-import com.spartronics4915.frc2025.commands.autos.AlignToReef.FieldBranchSide;
-import com.spartronics4915.frc2025.commands.autos.DriveToReefPoint;
-import com.spartronics4915.frc2025.commands.VariableAutos.BranchHeight;
-import com.spartronics4915.frc2025.commands.VariableAutos.BranchSide;
-import com.spartronics4915.frc2025.commands.VariableAutos.FieldBranch;
-import com.spartronics4915.frc2025.commands.VariableAutos.ReefSide;
-import com.spartronics4915.frc2025.commands.VariableAutos.StationSide;
-import com.spartronics4915.frc2025.commands.drive.ChassisSpeedSuppliers;
-import com.spartronics4915.frc2025.commands.drive.RotationIndependentControlCommand;
-import com.spartronics4915.frc2025.commands.drive.SwerveTeleopCommand;
 import com.spartronics4915.frc2025.subsystems.ClimberSubsystem;
 import com.spartronics4915.frc2025.subsystems.MechanismRenderer;
 import com.spartronics4915.frc2025.subsystems.MotorSimulationSubsystem;
-import com.spartronics4915.frc2025.subsystems.OdometrySubsystem;
 import com.spartronics4915.frc2025.subsystems.SwerveSubsystem;
 import com.spartronics4915.frc2025.subsystems.WinchClimber;
-import com.spartronics4915.frc2025.subsystems.bling2.*;
-import com.spartronics4915.frc2025.subsystems.vision.LimelightVisionSubsystem;
 import com.spartronics4915.frc2025.subsystems.coral.IntakeSubsystem;
 import com.spartronics4915.frc2025.subsystems.coral.ArmSubsystem;
 import com.spartronics4915.frc2025.subsystems.coral.ElevatorSubsystem;
-import com.spartronics4915.frc2025.subsystems.vision.SimVisionSubsystem;
-import com.spartronics4915.frc2025.subsystems.vision.VisionDeviceSubystem;
 import com.spartronics4915.frc2025.util.ModeSwitchHandler;
 import com.spartronics4915.frc2025.subsystems.coral.ElevatorSubsystem;
 
-import static com.spartronics4915.frc2025.commands.drive.ChassisSpeedSuppliers.shouldFlip;
 import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.Seconds;
 
@@ -94,29 +70,10 @@ public class RobotContainer {
 
     private static final AprilTagFieldLayout fieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded);
     
-    private final ElementLocator elementLocator = new ElementLocator();
-    private VisionDeviceSubystem visionSubsystem = null;
-    private OdometrySubsystem odometrySubsystem = null;
-    
     public final IntakeSubsystem intakeSubsystem;
     public final ArmSubsystem armSubsystem;
     public final ElevatorSubsystem elevatorSubsystem;
     public final WinchClimber climberSubsystem;
-
-    
-    public final DynamicsCommandFactory dynamics;
-
-    public SwerveTeleopCommand swerveTeleopCommand = null;
-    // Replace with CommandPS4Controller or CommandJoystick if needed
-    
-    public final BlingSubsystem blingSubsystem;
-    
-    private AlignToReef alignmentCommandFactory = null;
-    private VariableAutos variableAutoFactory = null;
-
-    private final SendableChooser<Command> autoChooser;
-
-    private final ComplexAutoChooser complexAutoChooser;
 
     /**
      * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -127,8 +84,6 @@ public class RobotContainer {
         armSubsystem = new ArmSubsystem();
         elevatorSubsystem = new ElevatorSubsystem();
         climberSubsystem = new WinchClimber();
-
-        dynamics = new DynamicsCommandFactory(armSubsystem, elevatorSubsystem, intakeSubsystem);
 
         ModeSwitchHandler.EnableModeSwitchHandler(
             intakeSubsystem,
@@ -160,31 +115,13 @@ public class RobotContainer {
             "setpoints"
         );
 
-        if (swerveSubsystem != null) {
-            swerveTeleopCommand = new SwerveTeleopCommand(driverController, swerveSubsystem);
-            alignmentCommandFactory = new AlignToReef(swerveSubsystem, fieldLayout);
-            variableAutoFactory = new VariableAutos(alignmentCommandFactory, dynamics, swerveSubsystem);
-            if (RobotBase.isSimulation()) {
-                visionSubsystem = new SimVisionSubsystem(swerveSubsystem);
-            } else {
-                visionSubsystem = new LimelightVisionSubsystem(swerveSubsystem, elementLocator.getFieldLayout());
-                ModeSwitchHandler.EnableModeSwitchHandler((LimelightVisionSubsystem) visionSubsystem);
-            }
-    
-            odometrySubsystem = new OdometrySubsystem(visionSubsystem, swerveSubsystem);
-        }
-
         // Configure the trigger bindings
         configureBindings();
 
-        complexAutoChooser = new ComplexAutoChooser(variableAutoFactory, 3);
+       
 
         // Need to initialize this here after vision is configured.
         // Need to clean up initialization flow to make it more clear
-        autoChooser =
-                buildAutoChooser();
-
-        blingSubsystem = new BlingSubsystem(0, BlingSegment.scrollingRainbow(42, 10));
     }
 
     /**
@@ -202,117 +139,10 @@ public class RobotContainer {
      * joysticks}.
      */
     private void configureBindings() {
-        
-
-        //#region driver controls
-
-        if (swerveSubsystem != null) {
-            swerveSubsystem.setDefaultCommand(swerveTeleopCommand);
-
-            driverController.leftStick().onTrue(Commands.runOnce(() -> {
-                swerveTeleopCommand.resetHeadingOffset();
-            }));
-
-            driverController.leftTrigger()
-                .whileTrue(
-                    Commands.run(swerveSubsystem::lockModules, swerveSubsystem)
-                    .withName("X Brake Swerve")
-                );
-
-            //this is a approximate version, we can do something more advanced by placing points at the center of the reef sides, then detecting which side it's closest to based on it's position
-            driverController.rightTrigger().whileTrue(
-                new RotationIndependentControlCommand(
-                    ChassisSpeedSuppliers.gotoAngle(ChassisSpeedSuppliers.orientTowardsNearestPOI(swerveSubsystem), swerveSubsystem),
-                    ChassisSpeedSuppliers.getSwerveTeleopCSSupplier(driverController.getHID(), swerveSubsystem),
-                    swerveSubsystem
-                )
-                .withName("Orient Towards Nearest POI")
-            );
-
-            driverController.b().toggleOnTrue(
-                Commands.startEnd(
-                    () -> {swerveTeleopCommand.setFieldRelative(!OI.kStartFieldRel);},
-                    () -> {swerveTeleopCommand.setFieldRelative(OI.kStartFieldRel);}
-                )
-                .withName("Toggle Field Relative")
-            );
-
-            driverController.a().onTrue(
-                Commands.defer(() -> {
-                    return Commands.runOnce(() -> {
-                        swerveTeleopCommand.setHeadingOffset(swerveSubsystem.getPose().getRotation());
-                    });
-                }, Set.of())
-            );
-
-            driverController.leftBumper().whileTrue(
-                alignmentCommandFactory.generateCommand(FieldBranchSide.LEFT)//.finallyDo((boolean interrupted) -> {
-                //     dynamics.gotoLastInputtedScore().onlyIf(() -> !interrupted);
-                // })
-                .withName("Align Left Branch")
-            );
-    
-            driverController.rightBumper().whileTrue(
-                alignmentCommandFactory.generateCommand(FieldBranchSide.RIGHT)//.finallyDo((boolean interrupted) -> {
-                //     dynamics.gotoLastInputtedScore().onlyIf(() -> !interrupted);
-                // })
-                .withName("Align Right Branch")
-            );
-
-            driverController.povUp().whileTrue(
-                Commands.run(() -> {
-                    swerveSubsystem.drive(new ChassisSpeeds(0.25, 0, 0));
-                })
-            );
-
-            driverController.povLeft().whileTrue(
-                Commands.run(() -> {
-                    swerveSubsystem.drive(new ChassisSpeeds(0, 0.25, 0));
-                })
-            );
-
-            driverController.povRight().whileTrue(
-                Commands.run(() -> {
-                    swerveSubsystem.drive(new ChassisSpeeds(0, -0.25, 0));
-                })
-            );
-
-            driverController.povDown().whileTrue(
-                Commands.run(() -> {
-                    swerveSubsystem.drive(new ChassisSpeeds(-0.25, 0, 0));
-                })
-            );
-        }
-
-        //#endregion
-
-        //#region automated controls
-
-        dynamics.hasScoredTrigger.onTrue(dynamics.stow());
-
-        new Trigger(intakeSubsystem::detect).and(DriverStation::isTeleop)
-            .debounce(0.02).onTrue(
-                Commands.parallel(
-                    dynamics.stow()
-                )
-            );
-
-        new Trigger(dynamics::funnelDetect).onTrue(
-            dynamics.intake()
-        );
 
         //#endregion
 
         //#region Operator Controls
-
-        operatorController.rightTrigger().onTrue( //whileTrue
-            dynamics.score()
-        );/*.onFalse(Commands.parallel(
-            intakeSubsystem.setPresetSpeedCommand(IntakeSpeed.NEUTRAL),
-            dynamics.stow()
-        ));*/
-
-        operatorController.leftTrigger().onTrue(dynamics.stow());
 
         operatorController.back().onTrue(
             intakeSubsystem.setPresetSpeedCommand(IntakeSpeed.FUNNEL_UNSTUCK)
@@ -320,38 +150,6 @@ public class RobotContainer {
             intakeSubsystem.setPresetSpeedCommand(IntakeSpeed.IN)
         ); //windows button
 
-        operatorController.y().onTrue(dynamics.operatorScore(DynaPreset.L4));
-
-        operatorController.x().onTrue(dynamics.operatorScore(DynaPreset.L3));
-
-        operatorController.b().onTrue(dynamics.operatorScore(DynaPreset.L2));
-
-        operatorController.start().onTrue(dynamics.intake()); //menu button
-
-        // operatorController.rightStick().whileTrue(
-        //     Commands.repeatingSequence(
-        //         Commands.sequence(
-        //             intakeSubsystem.setPresetSpeedCommand(IntakeSpeed.OUT),
-        //             Commands.waitSeconds(.25)
-        //         ).onlyIf(() -> !intakeSubsystem.detect()),
-        //         Commands.sequence(
-        //             intakeSubsystem.setPresetSpeedCommand(IntakeSpeed.IN),
-        //             Commands.waitSeconds(.25)
-        //         ).onlyIf(() -> !intakeSubsystem.detect())
-        //     )
-        //     ).onFalse(intakeSubsystem.setPresetSpeedCommand(IntakeSpeed.IN).onlyIf(() -> !intakeSubsystem.detect()));
-
-        operatorController.leftStick().onTrue(
-            dynamics.gotoScore(DynaPreset.ALGAE_HIGH)
-        );
-
-        operatorController.rightStick().onTrue(
-            dynamics.gotoScore(DynaPreset.ALGAE_LOW)
-        );
-
-        operatorController.a().onTrue(
-            dynamics.removeAlgaeArm()
-        );
 
         operatorController.povUp().whileTrue(elevatorSubsystem.manualMode(0.002));
 
@@ -373,18 +171,6 @@ public class RobotContainer {
 
         SmartDashboard.putData("setPreset1", armSubsystem.setMechanismAngleCommand(Rotation2d.fromDegrees(270)));
 
-        SmartDashboard.putData("stowLoad", dynamics.loadStow());
-        SmartDashboard.putData("stowPreScore", dynamics.prescoreStow());
-        SmartDashboard.putData("stow", dynamics.stow());
-        SmartDashboard.putData("score", dynamics.score());
-
-        SmartDashboard.putData("L1", dynamics.gotoScore(DynaPreset.L1));
-        SmartDashboard.putData("L2", dynamics.gotoScore(DynaPreset.L2));
-        SmartDashboard.putData("L3", dynamics.gotoScore(DynaPreset.L3));
-        SmartDashboard.putData("L4", dynamics.gotoScore(DynaPreset.L4));
-
-        SmartDashboard.putData("intake", dynamics.intake());
-
         SmartDashboard.putNumber("elevator setpoint", 0);
         SmartDashboard.putNumber("arm setpoint", 270);
 
@@ -402,14 +188,6 @@ public class RobotContainer {
      *
      * @return the command to run in autonomous
      */
-    public Command getAutonomousCommand() {
-
-        // return Autos.driveToNote(swerveSubsystem, noteDetector);
-        // return new DriveToReefPoint(swerveSubsystem, elementLocator, 11).generate();
-        // return complexAutoChooser.getAuto();
-        return autoChooser.getSelected();
-
-    }
 
     private SendableChooser<Command> buildAutoChooser() {
         SendableChooser<Command> chooser = new SendableChooser<Command>();
@@ -418,44 +196,6 @@ public class RobotContainer {
 
         chooser.setDefaultOption("None", Commands.none());
 
-        if (swerveSubsystem != null) {
-            var variableAuto = Commands.defer(complexAutoChooser::getAuto, Set.of(swerveSubsystem));
-            variableAuto.setName("variableAuto");
-
-            chooser.addOption("Create auto...", variableAuto);
-
-            // chooser.addOption("ReverseLeave", Autos.reverseForSeconds(swerveSubsystem, 3));
-            // chooser.addOption("Drive to Reef Point", new DriveToReefPoint(swerveSubsystem, elementLocator, 11).generate());
-            // chooser.addOption("M-R debug straight", new PathPlannerAuto("M-R straight debug"));
-            // chooser.addOption("M-R debug curve", new PathPlannerAuto("M-R curve debug"));
-            // chooser.addOption("M-R Circle", new PathPlannerAuto("Circle move debug"));
-            // chooser.addOption("Reef loop debug", new PathPlannerAuto("Reef loop debug"));
-            chooser.addOption("Leave", new PathPlannerAuto("Leave Auto"));
-
-            chooser.addOption("Test Single Run", Commands.sequence(
-                    dynamics.loadStow(),
-                    dynamics.blockingIntake(),
-                    Commands.defer(complexAutoChooser::getSingleRun, Set.of(swerveSubsystem))
-            ));
-
-            chooser.addOption("Align with move", Commands.sequence(
-                variableAutoFactory.generateAutoCycle(FieldBranch.A, StationSide.LEFT, BranchHeight.L4),
-                variableAutoFactory.generateAutoCycle(FieldBranch.C, StationSide.LEFT, BranchHeight.L4),
-                variableAutoFactory.generateAutoCycle(FieldBranch.E, StationSide.LEFT, BranchHeight.L4),
-                variableAutoFactory.generateAutoCycle(FieldBranch.G, StationSide.LEFT, BranchHeight.L4),
-                variableAutoFactory.generateAutoCycle(FieldBranch.I, StationSide.LEFT, BranchHeight.L4),
-                variableAutoFactory.generateAutoCycle(FieldBranch.K, StationSide.LEFT, BranchHeight.L4)
-            ));
-
-            chooser.addOption("Align Mirror with move", Commands.sequence(
-                variableAutoFactory.generateAutoCycle(FieldBranch.A, StationSide.RIGHT, BranchHeight.L4),
-                variableAutoFactory.generateAutoCycle(FieldBranch.C, StationSide.RIGHT, BranchHeight.L4),
-                variableAutoFactory.generateAutoCycle(FieldBranch.E, StationSide.RIGHT, BranchHeight.L4),
-                variableAutoFactory.generateAutoCycle(FieldBranch.G, StationSide.RIGHT, BranchHeight.L4),
-                variableAutoFactory.generateAutoCycle(FieldBranch.I, StationSide.RIGHT, BranchHeight.L4),
-                variableAutoFactory.generateAutoCycle(FieldBranch.K, StationSide.RIGHT, BranchHeight.L4)
-            ));
-        }
 
         chooser.onChange((c) -> {
             SmartDashboard.putBoolean("Using Variable Auto?", c.getName() == "variableAuto");
